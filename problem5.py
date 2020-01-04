f6cdf1b2bd71cf3f63b755b3de74fb671a535f42
Josh Nicholas <josh@thambi.li>
2020-01-04 16:47:27 +1000

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -2,17 +2,37 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"""
 
-num = 20
-divisible = True
-while divisible == True:
-    listo = []
-    for i in range(1,21):
-        result = num%i
-        if result == 0:
-            listo.append(i)
-    if len(listo) == 20:
-        divisible = False
-        print(listo)
-        print(f"num is {num}")
-    else:
-        num += 20
+## original (successful) solution:
+
+# num = 20
+# divisible = True
+# while divisible == True:
+#     listo = []
+#     for i in range(1,21):
+#         result = num%i
+#         if result == 0:
+#             listo.append(i)
+#     if len(listo) == 20:
+#         divisible = False
+#         print(listo)
+#         print(f"num is {num}")
+#     else:
+#         num += 20
+
+##############
+
+## after googling:
+
+num = 2520
+count = 1
+
+while True:
+    init = 0
+    for i in range(11,21):
+        if num%i == 0:
+            init += 1
+    if init == 10:
+        print(num)
+        break
+    num += 2520
+
